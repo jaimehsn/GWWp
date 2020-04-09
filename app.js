9776972e624969719656007678f81f12213015e1
jaimehsn <jaimehsn@gmail.com>
2020-04-09 22:23:30 +0200

    Refactorización y puesta en marcha de metodos de seguridad

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express")
 const session = require('express-session')
 const bodyParser = require("body-parser")
 const morgan = require('morgan')
-
 const app = express()
+const api = require("./app/routes")
+
 app.use(morgan('combined'))
 
 app.use(session({
@@ -19,16 +20,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // simple route
-require("./app/routes/user.routes")(app);
-require("./app/routes/note.routes")(app);
-require("./app/routes/auth.routes")(app);
+//require("./app/routes/user.routes")(app);
+//require("./app/routes/note.routes")(app);
+//require("./app/routes/auth.routes")(app);
+app.use('/api', api)
 
 app.get("/sesion", (req, res) => {
     res.send({ message: `${req.session.loggedin}` })
 })
-console.log("Sesión", session.log)
 
-// set port, listen for requests
-app.listen(9000, () => {
-    console.log("Server is running on port 9000.");
-})
+
+module.exports = app
---
+++ app/routes/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const api = express.Router()
+const users = require("../controllers/user.controller")
+const notes = require("../controllers/note.controller")
+const auth = require("../controllers/auth.controller")
+
+// Register an user
+api.post("/users", users.create)
+
+// Retrieve all Customers
+api.get("/users", users.findAll)
+
+// Retrieve a single Customer with customerId
+api.get("/users/:userMail", users.findOne)
+
+// Update a Customer with customerId
+api.put("/users/:userMail", users.update)
+
+// Delete a Customer with customerId
+api.delete("/users/:userMail", users.delete)
+
+// Log In
+api.post("/auth", auth.authentication)
+
+//Log Out 
+api.delete("/auth", auth.logout)
+
+api.post("/notes", (req, res) => {
+    if (req.session.loggedin) {
+        notes.create(req, res)
+    } else {
+        res.status(403).send({ message: `Unauthorized action` })
+    }
+})
+
+// Retrieve all Customers
+api.get("/notes/:codeGrp", notes.findAll)
+
+// Retrieve a single Customer with customerId
+api.get("/notes/:noteId", notes.findOne)
+
+// Update a Customer with customerId
+api.put("/notes/:noteId", notes.update)
+
+// Delete a Customer with customerId
+api.delete("/notes/delOnes/:noteId", notes.delete)
+
+// Create a new Customer
+api.delete("/notes/grpDel/:codeGrp", notes.deleteAll)
+
+module.exports = api
