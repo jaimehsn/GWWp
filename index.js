6ce26f82e5e98c14a0d517152b7d920ffa299be4
jaimehsn <jaimehsn@gmail.com>
2020-06-10 19:23:35 +0200

    Implementación de prueba correcta de Socket.io.

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const app = require("./app");
 const connect = require("./src/models/db");
 const fs = require('fs')
-const https = require("https")
+const https = require("https").createServer({
+  key: fs.readFileSync('./src/ssl/key.pem', 'utf8'),
+  cert: fs.readFileSync('./src/ssl/cert.pem', 'utf8'),
+  passphrase: 'L0sm0nt3sn0t13n3n0j0s'
+}, app)
+
+const io = require("socket.io")(https)
+
+io.on('connection', (socket) => {
+  console.log('**************USUARIO CONECTADO SOCKET**************');
+  socket.on('emit_method', (data) => {
+    io.emit('prueba', data);
+    console.log("DATOS RECIBIDOS DEL SOCKET: ", data);
+  });
+  socket.on('disconnect', () => {
+    console.log('**************USUARIO DESCONECTADO SOCKET**************');
+  });
+});
+
 
 //Test database
 
 connect.authenticate()
   .then(() => {
     console.log("Database connected...");
-    // set port, listen for requests
-    https.createServer({
-      key: fs.readFileSync('./src/ssl/key.pem'),
-      cert: fs.readFileSync('./src/ssl/cert.pem'),
-      passphrase: 'L0sm0nt3sn0t13n3n0j0s'
-    }, app).listen(9000, () => {
-      console.log("Server is running on port 9000.");
+    //Integrate socket
+    https.listen(443, () => {
+      console.log("Server is running on port 443.");
     });
   })
   .catch((err) => {
